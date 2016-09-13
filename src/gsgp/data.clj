7ac681c6ec81204903f04be2757c82088d3b7eda
Felipe Soares Gonçalves Rosa <felipe.sgrosa@gmail.com>
2016-09-13 12:03:08 -0300

    added gsgp.data docs

--- src/gsgp/data.clj
+++ src/gsgp/data.clj
@@ -4,11 +4,15 @@
 
 
 (defn load-txt
+  "Load a dataset from a .txt file.
+  The dataset's columns should be separated by whitespace."
   [file-name]
   (with-open [r (io/reader file-name)]
     (mapv #(read-string (str "[" % "]")) (line-seq r))))
 
 (defn save-txt
+  "Save a dataset to a .txt file.
+  Dataset columns will be separated by whitespace."
   [file-name data-set]
   (with-open [w (io/writer file-name)]
     (.write w (string/join "\n" (mapv #(string/join " " %) data-set)))))
