2db034d68f3c05e8c1bbe10cea340b6149041df9
Felipe Soares Gonçalves Rosa <felipe.sgrosa@gmail.com>
2016-09-18 00:17:08 -0300

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject gsgp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Geometric Semantic Genetic Programming library"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://github.com/FelipeRosa/clojure-gsgp/blob/master/LICENSE.md"}
   :local-repo "local-m2"
   :main gsgp.core
   :dependencies [[org.clojure/clojure "1.8.0"]])
